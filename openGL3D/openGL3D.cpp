1434aa45876579abdaf63855dcfce7df5021433b
Levi <leviamullins@gmail.com>
2025-11-24 02:15:18 -0500

    wsdefrg

--- openGL3D/openGL3D.cpp
+++ openGL3D/openGL3D.cpp
@@ -164,6 +164,7 @@ int main()
         std::cout << "Loaded model with: " << Gun.meshes.size() << " meshes\n";
     }
 
+
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
